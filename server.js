4764db8e31bcc4e5146efe35fff33c8f168d8aae
ErifiliTs <ErifiliTs@protonmail.com>
2021-09-25 20:14:48 +0300

    products backend

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const shortid = require("shortid");
+
+const app = express();
+app.use(express.json());
+
+const url = "mongodb://localhost/react_shopping_cart_db";
+mongoose.connect(url);
+
+const Product = mongoose.model(
+  "products",
+  new mongoose.Schema({
+    _id: { type: String, default: shortid.generate },
+    title: String,
+    description: String,
+    image: String,
+    price: Number,
+    availableSizes: [String],
+  })
+);
+
+app.get("/api/products", async (req, res) => {
+  const products = await Product.find({});
+  res.send(products);
+});
+
+app.post("/api/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  const savedProduct = await newProduct.save();
+  res.send(savedProduct);
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+  res.send(deletedProduct);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("serve at http://localhost:5000"));
--- src/App.js
+++ src/App.js
@@ -1,109 +1,130 @@
-import React from 'react'
-import './index.css';
-import data from "./data.json"
-import Products from './components/Products';
-import Filter from './components/Filter';
-import Cart from './components/Cart'
+import React from "react";
+import "./index.css";
+import data from "./data.json";
+import Products from "./components/Products";
+import Filter from "./components/Filter";
+import Cart from "./components/Cart";
 
 class App extends React.Component {
-constructor(){
-	super();
-	this.state = {
-		products: data.products,
-		cartItems: 
-		localStorage.getItem(this.cartItems) ? JSON.parse(localStorage.getItem(this.cartItems)) : [],
-		size:"",
-		sort: "",
-	};
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      cartItems: localStorage.getItem(this.cartItems)
+        ? JSON.parse(localStorage.getItem(this.cartItems))
+        : [],
+      size: "",
+      sort: "",
+    };
+  }
+
+  createOrder = (order) => {
+    alert("Need to save order for " + order.name);
+  };
+
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({
+      cartItems: cartItems.filter((x) => x._id !== product._id),
+    });
+    localStorage.setItem(
+      this.cartItems,
+      JSON.stringify(cartItems.filter((x) => x._id !== product._id))
+    );
+  };
+
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach((item) => {
+      if (item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if (!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 });
+    }
+    this.setState(
+      { cartItems },
+      localStorage.setItem(this.cartItems, JSON.stringify(cartItems))
+    );
+  };
+
+  sortProducts = (e) => {
+    const sort = e.target.value;
+    console.log(e.target.value);
+    this.setState((state) => ({
+      sort: sort,
+      products: state.products
+        .slice()
+        .sort((a, b) =>
+          sort === "Lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === "Highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id > b._id
+            ? 1
+            : -1
+        ),
+    }));
+  };
+
+  filterProducts = (e) => {
+    console.log(e.target.value);
+    if (e.target.value === "") {
+      this.setState({
+        size: e.target.value,
+        products: data.products,
+      });
+    } else {
+      this.setState({
+        size: e.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(e.target.value) >= 0
+        ),
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className='grid-container'>
+        <header>
+          <a href='/'>React Shopping Cart</a>
+        </header>
+        <main>
+          <div className='content'>
+            <div className='main'>
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
+              ></Filter>
+              <Products
+                products={this.state.products}
+                addToCart={this.addToCart}
+              ></Products>
+            </div>
+            <div className='sidebar'>
+              <Cart
+                cartItems={this.state.cartItems}
+                removeFromCart={this.removeFromCart}
+                createOrder={this.createOrder}
+              />
+            </div>
+          </div>
+        </main>
+        <footer>All right is reserved.</footer>
+      </div>
+    );
+  }
 }
-
-createOrder = (order) => {
-	alert("Need to save order for " + order.name);
-}
-
-removeFromCart = (product) => {
-	const cartItems = this.state.cartItems.slice();
-	this.setState({cartItems: cartItems.filter(x => x._id !== product._id), })
-	localStorage.setItem(this.cartItems, JSON.stringify(cartItems.filter(x => x._id !== product._id)));
-
-}
-
-addToCart = (product) => {
-	const cartItems = this.state.cartItems.slice();
-	let alreadyInCart = false;
-	cartItems.forEach(item => {
-		if(item._id === product._id){
-			item.count++;
-			alreadyInCart = true;
-		}
-	});
-	if(!alreadyInCart){
-		cartItems.push({...product, count: 1})
-	}
-	this.setState({cartItems},
-	localStorage.setItem(this.cartItems, JSON.stringify(cartItems)));
-}
-
-sortProducts = (e) => {
-const sort = e.target.value;
-console.log(e.target.value);
-this.setState((state) => ({
-	sort: sort,
-	products: state.products.slice().sort((a, b) => (
-		sort ===  "Lowest" ? ((a.price > b.price) ? 1:-1):
-		sort ===  "Highest" ? ((a.price < b.price) ? 1:-1):
-		((a._id > b._id) ? 1:-1)
-		
-	))
-}))}
-
-filterProducts = (e) => {
-	console.log(e.target.value);
-	if(e.target.value === ""){
-		this.setState({
-			size: e.target.value, 
-			products:data.products});
-	}
-	else{
-		this.setState({
-			size: e.target.value,
-			products: data.products.filter(product => product.availableSizes.indexOf(e.target.value) >= 0),
-		})
-	}
-}
-
-render () {
-return (
-	<div className="grid-container">
-		<header>
-		<a href="/">React Shopping Cart</a>
-		</header>
-		<main>
-			<div className="content">
-				<div className="main">
-					<Filter 
-						count={this.state.products.length} 
-						size={this.state.size} 
-						sort={this.state.sort} 
-						filterProducts={this.filterProducts} 
-						sortProducts={this.sortProducts}>
-					</Filter>
-					<Products 
-						products={this.state.products} addToCart={this.addToCart}>
-					</Products>
-				</div>
-				<div className="sidebar">
-					<Cart 
-						cartItems={this.state.cartItems} removeFromCart={this.removeFromCart}
-						createOrder={this.createOrder}
-					/>
-				</div>
-			</div>
-		</main>
-		<footer>
-			All right is reserved.
-		</footer>
-	</div>
-)}}
 
 export default App;
